2f0fce82d844b2d15ad344d08246e87aac7909ef
Richard Hong <rhong24@gmail.com>
2023-07-14 12:50:21 -0500

    updating init script within backend to typescript file, updating tsconfig on backend to include all ts files, updating index.tsx within frontend for the effective main page of app (still WIP)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,82 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import { NextPage } from "next";
-import { Container } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import {
+  MediaRenderer,
+  useContract,
+  useContractRead,
+} from "@thirdweb-dev/react";
+import { HERO_IMAGE_URL, LOTTERY_CONTRACT_ADDRESS } from "../const/addresses";
+import { ethers } from "ethers";
 
 const Home: NextPage = () => {
+  const { contract } = useContract(LOTTERY_CONTRACT_ADDRESS);
+
+  const { data: entryCost, isLoading: isLoadingEntryCost } = useContractRead(
+    contract,
+    "entryCost",
+  );
+
+  const entryCostInEther = entryCost
+    ? ethers.utils.formatEther(entryCost)
+    : "0";
+
   return (
     <Container
       maxW={"1440px"}
       py={8}
-    ></Container>
+    >
+      <SimpleGrid
+        columns={2}
+        spacing={10}
+        minH={"60vh"}
+      >
+        <Flex>
+          <MediaRenderer
+            src={HERO_IMAGE_URL}
+            width='100%'
+            height='100%'
+          />
+        </Flex>
+        <Flex
+          justifyContent={"center"}
+          alignItems={"center"}
+          p={"5%"}
+        >
+          <Stack spacing={10}>
+            <Box>
+              <Text fontSize={"xl"}>Raffle App</Text>
+              <Text
+                fontSize={"4xl"}
+                fontWeight={"bold"}
+              >
+                Buy a ticket to win the NFT Prize
+              </Text>
+            </Box>
+            <Text fontSize={"xl"}>
+              Buy entries for a chance to win the NFT! The NFT will be
+              transferred to the winner&apos;s address. The more entries you
+              buy, the higher the chance you have of winning the prize!
+            </Text>
+
+            {!isLoadingEntryCost && (
+              <Text
+                fontSize={"2xl"}
+                fontWeight={"bold"}
+              >
+                Cost Per Entry: {entryCostInEther} ETH
+              </Text>
+            )}
+          </Stack>
+        </Flex>
+      </SimpleGrid>
+    </Container>
   );
 };
 
