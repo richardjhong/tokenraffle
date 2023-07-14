8c6cd0b8614652ec30cb7c6ee72ce118cff5a286
Richard Hong <rhong24@gmail.com>
2023-07-13 20:51:53 -0500

    updating starter code for _app page to use sepolia for network as well as resolve typescript error messages, installing chakra-ui to keep same consistency of styling as tutorial follow along for now, may look into migrating into tailwind css later on

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react/evm";
 import "../styles/globals.css";
+import Sepolia from "@thirdweb-dev/chains";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = Sepolia;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
