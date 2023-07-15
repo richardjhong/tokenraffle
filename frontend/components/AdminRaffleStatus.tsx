e735688d1dd8a64df2b515fbc043ee08892c3afd
Richard Hong <rhong24@gmail.com>
2023-07-14 20:39:48 -0500

    adding ui elements to allow contract owner to open a raffle on an nft that is owned by contract as well as ending raffle if the raffle is ongoing

--- frontend/components/AdminRaffleStatus.tsx
+++ frontend/components/AdminRaffleStatus.tsx
@@ -1,7 +1,7 @@
-import { useContract, useContractRead } from "@thirdweb-dev/react";
+import { Web3Button, useContract, useContractRead } from "@thirdweb-dev/react";
 import { RAFFLE_CONTRACT_ADDRESS } from "../const/addresses";
 import React, { useState } from "react";
-import { Card, Text } from "@chakra-ui/react";
+import { Box, Card, Input, Stack, Text } from "@chakra-ui/react";
 import RaffleStatus from "./RaffleStatus";
 
 const AdminRaffleStatus = () => {
@@ -31,6 +31,49 @@ const AdminRaffleStatus = () => {
         Raffle Status:
       </Text>
       <RaffleStatus raffleStatus={raffleStatus} />
+      {!raffleStatus ? (
+        <Stack
+          spacing={4}
+          mt={4}
+        >
+          <Box>
+            <Text>Prize Contract Address:</Text>
+            <Input
+              placeholder={"0x..."}
+              type='text'
+              value={nftContractAddress}
+              onChange={(e) => setNftContractAddress(e.target.value)}
+            />
+          </Box>
+          <Box>
+            <Text>Prize Token ID:</Text>
+            <Input
+              placeholder={"0"}
+              type='number'
+              value={tokenId}
+              onChange={(e) => setTokenId(parseInt(e.target.value))}
+            />
+          </Box>
+          <Web3Button
+            contractAddress={RAFFLE_CONTRACT_ADDRESS}
+            action={(contract) =>
+              contract.call("startRaffle", [nftContractAddress, tokenId])
+            }
+            onSuccess={reset}
+          >
+            Start Raffle
+          </Web3Button>
+        </Stack>
+      ) : (
+        <Stack mt={4}>
+          <Web3Button
+            contractAddress={RAFFLE_CONTRACT_ADDRESS}
+            action={(contract) => contract.call("endRaffle")}
+          >
+            End Raffle
+          </Web3Button>
+        </Stack>
+      )}
     </Card>
   );
 };
